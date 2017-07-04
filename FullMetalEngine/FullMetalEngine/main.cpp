54b2f9974daad2eab4fb236a5fbf715670a27332
Alex Soto <alexsotodev@gmail.com>
2017-07-03 20:07:57 -0500

    Game loop work started. Not a real commit. Just moving from laptop to desktop sync. Lots of reading/research Ch 7 of Game Engine Architecture

--- FullMetalEngine/FullMetalEngine/main.cpp
+++ FullMetalEngine/FullMetalEngine/main.cpp
@@ -1,9 +1,59 @@
 #include "DebugLog.h"
+#include <chrono>
+#include <WinUser.h>
 
 int main()
 {
-	DebugLog::DebugOut("Hello\n");
-	int x = 5;
-	DebugLog::DebugOut("Hello %d\n", x);
-	return 0;
+	//Setup systems/initialize prereq work
+	Initialize();
+
+	bool quit = false;
+
+	//Main game loop
+	while (!quit)
+	{
+		ReadInput();
+		Update();
+		Render();
+
+	}
+	
+	//Properly shutdown systems and close application
+	Terminate();
+
+
+
+
+
+	
+	//DebugLog::DebugOut("Hello\n");
+	//int x = 5;
+	//DebugLog::DebugOut("Hello %d\n", x);
+	//return 0;
+}
+
+void Initialize()
+{
+
+}
+
+void ReadInput()
+{
+	
+}
+
+void Update()
+{
+	DebugLog::DebugOut("Update\n");
+}
+
+void Render()
+{
+	DebugLog::DebugOut("Render");
+}
+
+
+void Terminate()
+{
+
 }
